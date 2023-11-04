e2a8ee3dd348349d1bf389a619c9c00068b2b665
amjed-ali-k <amjedmgm@gmail.com>
2023-11-04 17:08:28 +0530

    🎉 init(*): Initialize
    Shadcn
    nextjs
    prisma
    nextauth

---
+++ README.md
@@ -0,0 +1,53 @@
+# TC Issuer
+
+An open source application built using the new router, server components and everything new in Next.js 13.
+
+> **Warning**
+> This app is a work in progress. I'm building this in public.
+<!-- > See the roadmap below. -->
+
+## About this project
+
+This project as an experiment to see how a modern app (with features like authentication, subscriptions, API routes, static pages for docs ...etc) would work in Next.js 13 and server components.
+
+## Features
+
+- New `/app` dir,
+- Routing, Layouts, Nested Layouts and Layout Groups
+- Data Fetching, Caching and Mutation
+- Loading UI
+- Route handlers
+- Metadata files
+- Server and Client Components
+- API Routes and Middlewares
+- Authentication using **NextAuth.js**
+- ORM using **Prisma**
+- Database on **PlanetScale**
+- UI Components built using **Radix UI**
+- Styled using **Tailwind CSS**
+- Validations using **Zod**
+- Written in **TypeScript**
+
+## Running Locally
+
+1. Install dependencies using pnpm:
+
+```sh
+pnpm install
+```
+
+2. Copy `.env.example` to `.env.local` and update the variables.
+
+```sh
+cp .env.example .env.local
+```
+
+3. Start the development server:
+
+```sh
+pnpm dev
+```
+
+## License
+
+Licensed under the [MIT license](https://github.com/amjed-ali-k/tc-issuer/blob/main/LICENSE.md).
---
+++ lib/auth.ts
@@ -0,0 +1,144 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { NextAuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import { env } from "@/env.mjs"
+import { db } from "@/lib/db"
+import bcrypt from "bcrypt"
+// const postmarkClient = new Client(env.POSTMARK_API_TOKEN)
+
+export const authOptions: NextAuthOptions = {
+  // huh any! I know.
+  // This is a temporary fix for prisma client.
+  // @see https://github.com/prisma/prisma/issues/16117
+  adapter: PrismaAdapter(db as any),
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  providers: [
+    CredentialsProvider({
+    // The name to display on the sign in form (e.g. "Sign in with...")
+    name: "Credentials",
+    // `credentials` is used to generate a form on the sign in page.
+    // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+    // e.g. domain, username, password, 2FA token, etc.
+    // You can pass any HTML attribute to the <input> tag through the object.
+    credentials: {
+      username: { label: "Username", type: "text", placeholder: "jsmith" },
+      password: { label: "Password", type: "password" }
+    },
+    async authorize(credentials, req) {
+      // Add logic here to look up the user from the credentials supplied
+      if (!credentials || !credentials.username || !credentials.password) {
+        return null
+      }
+
+     const user = await db.user.findFirst({
+        where: {
+          email: credentials.username,
+        },
+      })
+
+      // verify password
+      try {
+      const res = await bcrypt.compare(credentials.password, user.password)
+        if (!res) return null
+
+      } catch (error) {
+        return null
+      }
+
+      if (user) {
+        // Any object returned will be saved in `user` property of the JWT
+        return user
+      } else {
+        // If you return null then an error will be displayed advising the user to check their details.
+        return null
+        // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+      }
+    }
+  })
+    // GitHubProvider({
+    //   clientId: env.GITHUB_CLIENT_ID,
+    //   clientSecret: env.GITHUB_CLIENT_SECRET,
+    // }),
+    // EmailProvider({
+    //   from: env.SMTP_FROM,
+    //   sendVerificationRequest: async ({ identifier, url, provider }) => {
+    //     const user = await db.user.findUnique({
+    //       where: {
+    //         email: identifier,
+    //       },
+    //       select: {
+    //         emailVerified: true,
+    //       },
+    //     })
+
+    //     const templateId = user?.emailVerified
+    //       ? env.POSTMARK_SIGN_IN_TEMPLATE
+    //       : env.POSTMARK_ACTIVATION_TEMPLATE
+    //     if (!templateId) {
+    //       throw new Error("Missing template id")
+    //     }
+
+    //     const result = await postmarkClient.sendEmailWithTemplate({
+    //       TemplateId: parseInt(templateId),
+    //       To: identifier,
+    //       From: provider.from as string,
+    //       TemplateModel: {
+    //         action_url: url,
+    //         product_name: siteConfig.name,
+    //       },
+    //       Headers: [
+    //         {
+    //           // Set this to prevent Gmail from threading emails.
+    //           // See https://stackoverflow.com/questions/23434110/force-emails-not-to-be-grouped-into-conversations/25435722.
+    //           Name: "X-Entity-Ref-ID",
+    //           Value: new Date().getTime() + "",
+    //         },
+    //       ],
+    //     })
+
+    //     if (result.ErrorCode) {
+    //       throw new Error(result.Message)
+    //     }
+    //   },
+    // }),
+  ],
+  callbacks: {
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id
+        session.user.name = token.name
+        session.user.email = token.email
+        session.user.image = token.picture
+      }
+
+      return session
+    },
+    async jwt({ token, user }) {
+      const dbUser = await db.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      })
+
+      if (!dbUser) {
+        if (user) {
+          token.id = user?.id
+        }
+        return token
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      }
+    },
+  },
+}
