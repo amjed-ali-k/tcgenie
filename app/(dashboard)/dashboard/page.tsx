b44ce2811c48e7d5f067a495e232de2cbf74213b
amjed-ali-k <amjedmgm@gmail.com>
2023-11-05 20:45:10 +0530

    Add Lucide-React components and update dashboard page

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation"
 import format from "date-fns/format"
-import { Users2 } from "lucide-react"
+import { ScrollText, Upload, Users2 } from "lucide-react"
 
 import { authOptions } from "@/lib/auth"
 import { db } from "@/lib/db"
@@ -9,6 +9,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { DashboardHeader } from "@/components/header"
 import { DashboardShell } from "@/components/shell"
 
+// Metadata for the page
 export const metadata = {
   title: "Dashboard",
 }
@@ -16,10 +17,12 @@ export const metadata = {
 export default async function DashboardPage() {
   const user = await getCurrentUser()
 
+  // If there is no user (i.e., the user is not logged in), redirect to the sign-in page
   if (!user) {
     redirect(authOptions?.pages?.signIn || "/login")
   }
 
+  // Get the status of the current user
   const status = await db.status.findUnique({
     where: { userId: user.id },
   })
@@ -39,9 +42,11 @@ export default async function DashboardPage() {
               <Users2 className="h-4 w-4 text-muted-foreground" />
             </CardHeader>
             <CardContent>
-              <div className="text-2xl font-bold">{status?.totalStudents}</div>
+              <div className="text-2xl font-bold">
+                {status?.totalStudents || 0}
+              </div>
               <p className="text-xs text-muted-foreground">
-                + {status?.newStudentsThisMonth} new students this month
+                + {status?.newStudentsThisMonth || 0} new students this month
               </p>
             </CardContent>
           </Card>
@@ -50,27 +55,31 @@ export default async function DashboardPage() {
               <CardTitle className="text-sm font-medium">
                 Total Issued
               </CardTitle>
-              <Users2 className="h-4 w-4 text-muted-foreground" />
+              <ScrollText className="h-4 w-4 text-muted-foreground" />
             </CardHeader>
             <CardContent>
-              <div className="text-2xl font-bold">{status?.totalTCIssued}</div>
+              <div className="text-2xl font-bold">
+                {status?.totalTCIssued || 0}
+              </div>
               <p className="text-xs text-muted-foreground">
-                + {status?.totalTCIssuedThisMonth} tc issues this month
+                + {status?.totalTCIssuedThisMonth || 0} tc issues this month
               </p>
             </CardContent>
           </Card>
           <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
               <CardTitle className="text-sm font-medium">Total Login</CardTitle>
-              <Users2 className="h-4 w-4 text-muted-foreground" />
+              <Upload className="h-4 w-4 text-muted-foreground" />
             </CardHeader>
             <CardContent>
-              <div className="text-2xl font-bold">{status?.totalLogins}</div>
+              <div className="text-2xl font-bold">
+                {status?.totalLogins || 0}
+              </div>
               <p className="text-xs text-muted-foreground">
                 Last logged in on{" "}
                 {format(
                   status?.lastLoginAt || new Date(),
-                  "dd-MM-yyyy hh:mm A"
+                  "dd-MM-yyyy hh:mm a"
                 )}
               </p>
             </CardContent>
