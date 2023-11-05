3f9c9edf219e88b532efa60d30886c19e3b7c6cb
amjed-ali-k <amjedmgm@gmail.com>
2023-11-05 13:18:40 +0530

    Refactor code for improved readability and maintainability.

---
+++ app/(dashboard)/dashboard/students/new-batch/student-form.tsx
@@ -0,0 +1,79 @@
+"use client"
+
+import React from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  name: z.string(),
+  admissionNo: z.string(),
+  parantName: z.string(),
+  parantAddress: z.string(),
+  phoneNo: z.string(),
+  caste: z.string(),
+  religion: z.string(),
+  dateOfBirth: z.string(),
+  dateOfAdmission: z.string(),
+  class: z.string(),
+  previousInstitution: z.string(),
+  tcRecievedDate: z.string(),
+  tcRecievedNo: z.string(),
+  dateOfLeaving: z.string(),
+  reasonForLeaving: z.string(),
+})
+
+function StudentForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+  return (
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  )
+}
+
+export default StudentForm
