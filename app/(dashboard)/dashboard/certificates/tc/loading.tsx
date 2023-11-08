7e4c03677a57cd85ffaafdd15a7ccb06e483e96a
amjed-ali-k <amjedmgm@gmail.com>
2023-11-08 19:50:04 +0530

    Refactor code for better performance and readability.

---
+++ app/(dashboard)/dashboard/certificates/tc/loading.tsx
@@ -0,0 +1,24 @@
+import { DashboardHeader } from "@/components/header"
+import { PostCreateButton } from "@/components/post-create-button"
+import { PostItem } from "@/components/post-item"
+import { DashboardShell } from "@/components/shell"
+
+export default function DashboardLoading() {
+  return (
+    <DashboardShell>
+      <DashboardHeader
+        heading="Generate TC and CC"
+        text="Generate certificates."
+      >
+        {/* <PostCreateButton /> */}
+      </DashboardHeader>
+      <div className="divide-border-200 divide-y rounded-md border">
+        <PostItem.Skeleton />
+        <PostItem.Skeleton />
+        <PostItem.Skeleton />
+        <PostItem.Skeleton />
+        <PostItem.Skeleton />
+      </div>
+    </DashboardShell>
+  )
+}
